30269d005291087cb4f4abd4308fbe341bcaebce
Kevin Forhan <kevin4forhan@gmail.com>
2021-01-24 11:39:25 -0600

    feat: added notificatoins and pop ups

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from "react";
-import "./App.css";
 import Header from "./components/Header";
-import Figure from './components/Figure'
+import Figure from "./components/Figure";
+import WrongLetters from "./components/WrongLetters";
+import Word from "./components/Word";
+
+
+import "./App.css";
+
 
 function App() {
   return (
     <div>
       <Header />
       <div className="game-container">
-<Figure />
+        <Figure />
+        <WrongLetters />
+        <Word />
       </div>
     </div>
   );
-} 
+}
 
 export default App;
---
+++ src/components/Notification.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Notification = () => {
+    return (
+        <div class="notification-container" id="notification-container">
+      <p> Oh no you've already entered this letter :(</p>
+    </div>
+    )
+}
+
+export default Notification
