0091e4eadd1c6ae3a2bd8305706c3fec0abb440f
Kevin Forhan <kevin4forhan@gmail.com>
2021-01-24 11:27:37 -0600

    feat: added Figure and styling

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import react from 'react'
+
+const Header = () => {
+    return (
+        <div>
+       <h1>
+           Hangman
+       </h1>
+       <p>Let's play Hangman, enter a letter to find the hidden word</p>
+       </div>
+    )
+}
+
+export default Header
